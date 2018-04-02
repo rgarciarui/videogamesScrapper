62ad22c6baf225f4176d7123985269fd66fd26a2
GarciaRuiz-Ricardo <rgarciarui@uoc.edu>
2018-04-02 20:13:24 +0200

    Actualización de codigo para el core del bucle de extración de datos de RetroCollect

--- src/videogamesScraper.R
+++ src/videogamesScraper.R
@@ -50,7 +50,7 @@ alt2 = html_nodes(table_start, xpath='//td[contains(@class, "searchResultsTableC
 
 # En este paso se eliminan los textos 'alt=', las comillas, y los espacios en 
 # blanco al comienzo de cada texto
-alt2 = gsub("* ", "", gsub('\\"',"", gsub('alt=',"",alt2),))
+alt2 = gsub("* ", "", gsub('\\"',"", gsub('alt=',"",alt2)))
 
 # Finalmente se eliminan los items que no corresponden con los valores 
 # alternativos de las imagenes, es decir: "Europe", "US", "Japan"
@@ -63,9 +63,27 @@ results$`Original System` = alt2
 # y cada nuevo data frame se unira al final del primero
 for(url_step in urls){
   
+  # llenamos la url, generando el data frame original
   url_0 <- read_html(url_step)
   table_0 <- html_nodes(url_0, css = "table")
   result_0 = html_table(table_0, header = TRUE, fill = TRUE)[[1]]
+  
+  # Se carga en una variable 'alt2' el conjunto de resultados de extraer todos los textos
+  # que se encuentran en los tag 'alt' en la pagina html cargada.
+  # El resultado es mucho mayor que el numero de 200 valores que se deben recoger
+  # por lo que se sigue con un proceso de limpieza.
+  alt_step = html_nodes(table_0, xpath='//td[contains(@class, "searchResultsTableCenterAlign")]//img/@alt')
+  
+  # En este paso se eliminan los textos 'alt=', las comillas, y los espacios en 
+  # blanco al comienzo de cada texto
+  alt_step = gsub("* ", "", gsub('\\"',"", gsub('alt=',"",alt_step)))
+  
+  # Finalmente se eliminan los items que no corresponden con los valores 
+  # alternativos de las imagenes, es decir: "Europe", "US", "Japan"
+  alt_step = alt_step[! alt_step %in% c("Europe", "US", "Japan")]
+  
+  # Despues del proceso de limpieza, se procede a cargar los datos correctos en el dataframe
+  result_0$`Original System` = alt_step
   
   # Hacemos un merge tomando en consideración que son dataframes iguales en columnas
   results = rbind(results, result_0)
@@ -73,7 +91,7 @@ for(url_step in urls){
 }
 
 # Eliminamos los objetos que ya no nos son necesarios para ajustar la memoria usada
-rm(url_0, table_0, result_0, table_start, url_start, urls, url_direccion, url_step)
+rm(url_0, table_0, result_0, table_start, url_start, urls, url_direccion, url_step, alt2)
 
 
 #Finalmente exportamos el data frame como un fichero cvs
